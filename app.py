3a938020018b73ecf23526f1f05c580f08764f40
Felipe Meganha <felipengmec@gmail.com>
2021-11-30 18:32:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+
 from webcam import webcam
 
+st.title("Webcam capture component")
+
+st.write("""
+- Accesses the user's webcam and displays the video feed in the browser.
+- Click the "Capture Frame" button to grab the current video frame and
+return it to Streamlit.
+""")
 captured_image = webcam()
 if captured_image is None:
     st.write("Waiting for capture...")
