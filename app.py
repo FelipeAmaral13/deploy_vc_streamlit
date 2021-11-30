b7abe34661f5c70b05c1a8e3f64484dc496e1cbd
Felipe Meganha <felipengmec@gmail.com>
2021-11-30 19:22:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,86 @@
+# main.py
+
+import cv2
 import streamlit as st
+import numpy as np
+import os
 
-from webcam import webcam
+def main():
 
-st.title("Webcam capture component")
+    selected_box = st.sidebar.selectbox(
+    'Escolha as seguintes opções',
+    ('Welcome','Segmentar cor', 'Cartoon')
+    )
 
-st.write("""
-- Accesses the user's webcam and displays the video feed in the browser.
-- Click the "Capture Frame" button to grab the current video frame and
-return it to Streamlit.
-""")
-captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+    if selected_box == 'Welcome':
+        welcome() 
+    if selected_box == 'Segmentar cor':
+        seg_color()
+    if selected_box == 'Cartoon':
+        cartoon()
+
+def welcome():
+    
+    st.title('Deploy de uma sistema de Visão Computacional')
+    
+    st.subheader('Um simples deploy de um sistema de visão Computacional'\
+                 ' utilizando Python e Streamlit.')
+    st.write(' Escolha no menu Lateral qual ferramenta usar:')
+    st.write('1) Welcome')
+    st.write('2) Segmentar Cor')
+    st.write('3) Cartoon')
+
+
+def seg_color():
+
+    st.title("Webcam Segementation Color")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+
+    cam = cv2.VideoCapture(0)       
+
+    while run:        
+        ret, frame = cam.read()
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)               
+
+        # Mascara
+        lower_blue = np.array([100, 120, 80])
+        upper_blue = np.array([255, 255, 255])
+        # Aplicando a mascara
+        mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+        frame_RGB = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+
+        FRAME_WINDOW.image(frame_RGB)
+    else:
+        st.write('Sem Acesso a Camera')
+
+
+
+def cartoon():
+    st.title("Cartoon")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+
+    cam = cv2.VideoCapture(0)       
+
+    while run:        
+        ret, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)               
+
+        # Edges
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        gray = cv2.medianBlur(gray, 5)
+        edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+
+        # Cartoon
+        color = cv2.bilateralFilter(frame, 9, 250, 250)
+        carton = cv2.bitwise_and(color, color, mask=edges)
+
+        FRAME_WINDOW.image(carton)
+    else:
+        st.write('Sem Acesso a Camera')
+
+
+if __name__ == "__main__":
+    main()
